3a40d98b73102c4bb73dad421ab39ab4b3800b03
Mohammad Kowsar <88027531+Kowsar14238@users.noreply.github.com>
2023-10-21 22:31:50 +0600

    Add files via upload

---
+++ 30 Days of JavaScript/toBeOrNot.js
@@ -0,0 +1,22 @@
+
+var expect = function(val) {
+    
+    return{
+        toBe: function(val2){
+            if(val2 !== val){
+                throw new Error("Not Equal");
+            }
+            else{
+                return true;
+            }
+        },
+        notToBe: function(val2){
+            if(val2 === val){
+                throw new Error("Equal");
+            }
+            else{
+                return true;
+            }
+        }
+    }
+};

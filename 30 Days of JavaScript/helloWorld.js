3dc71004759fc25122e566fc245a241d39f3ff80
Mohammad Kowsar <88027531+Kowsar14238@users.noreply.github.com>
2023-10-28 21:17:52 +0600

    Delete 30 Days of JavaScript/helloWorld.js

--- 30 Days of JavaScript/helloWorld.js
+++
@@ -1,9 +0,0 @@
-var createHelloWorld = function(args) {
-    return function(...innerArgs) {
-        return "Hello World";
-    }
-};
-
-var args = [];
-var result = createHelloWorld(args);
-console.log(result()); // Output: "Hello World"
